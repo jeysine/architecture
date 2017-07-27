da943af381cacc29ebb87443287a0b27aa61a666
lloo0331 <lloo0331oo@126.com>
2017-07-27 18:32:04 +0800

    模板生成,未完工

---
+++ architecture-common/src/main/java/utils/code/generate/entity/BaseEntity.java
@@ -0,0 +1,9 @@
+package utils.code.generate.entity;
+
+/**
+ * Created by li on 2017/7/18.
+ * 克隆工具类
+ */
+
+public class BaseEntity {
+}
---
+++ architecture-common/src/main/java/utils/code/generate/gentype/AbsGenType.java
@@ -0,0 +1,10 @@
+package utils.code.generate.gentype;
+
+import utils.code.generate.entity.BaseEntity;
+
+public abstract class AbsGenType {
+
+    public abstract void generate(BaseEntity baseEntity);
+
+
+}
