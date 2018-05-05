f797d1b7d3dc5f57e1bf8589774a18c471bc1ba3
GMLDD <lloo0331oo@126.com>
2018-05-05 21:06:16 +0800

    akka的集群demo

---
+++ architecture-common/src/main/java/utils/akka/cluster/AkkaCluster1.java
@@ -0,0 +1,18 @@
+package utils.akka.cluster;
+/**
+ * Created by li on 2018/4/28.
+ */
+
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.typesafe.config.ConfigFactory;
+
+public class AkkaCluster1 {
+
+    public static void main(String [] args){
+        System.out.println("Start simpleClusterListener");
+        ActorSystem system = ActorSystem.create("akkaClusterTest", ConfigFactory.load("akkaCluster1.conf"));
+        system.actorOf(Props.create(SimpleClusterListener.class), "simpleClusterListener");
+        System.out.println("Started simpleClusterListener");
+    }
+}
